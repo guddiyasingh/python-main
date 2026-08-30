f0511e8f0425a0bc081e14e51771ca61365f4f3c
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-08-30 22:53:52 +0530

    add multiple routes in nano.py file

--- FlaskFramework/nano.py
+++ FlaskFramework/nano.py
@@ -1,9 +1,31 @@
-from flask import Flask
+# from flask import Flask
 
+# app = Flask(__name__)
+# @app.route("/")
+# def home():
+#     return "Hello, Guddiya!"
+
+# if __name__ == "__main__":
+#     app.run(debug= True)    
+
+    # Create another page
+    #  we can create multiple routes:
+
+    
+from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "Hello, Guddiya!"
+    return "Home Page"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact Page"    
 
 if __name__ == "__main__":
-    app.run(debug= True)    
+    app.run(debug=True)
