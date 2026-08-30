c59dae2a692b3297f59500595235724b16f792cb
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-08-30 23:25:25 +0530

    add flask with pandas

--- FlaskFramework/nano.py
+++ FlaskFramework/nano.py
@@ -11,7 +11,7 @@
     # Create another page
     #  we can create multiple routes:
 
-    
+import pandas as pd   
 from flask import Flask
 app = Flask(__name__)
 
@@ -25,7 +25,45 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return "Contact Page"    
+    return "Contact Page"  
+
+    df = pd.DataFram({
+    "Name":["Guddiya","Aman","Riya"],
+    "Marks":[90,85,95]
+})
+
+@app.route("/students")
+def students():
+    return df.to_html(index=False)
+  
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+# Flask + Pandas
+# This is where Flask becomes particularly interesting for your
+# Data Analysis learning.
+
+# import pandas as pd
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# df = Flask(__name__)
+
+# df = pd.DataFram({
+#     "Name":["Guddiya","Aman","Riya"],
+#     "Marks":[90,85,95]
+# })
+
+# @app.route("/students")
+# def students():
+#     return df.to_html(index=False)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)    
+  
