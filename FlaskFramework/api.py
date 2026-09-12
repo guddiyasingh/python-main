d011167260fba4a7b384ed4f58083fc56cbca53f
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-09-12 23:10:56 +0530

    add some code in api file

--- FlaskFramework/api.py
+++ FlaskFramework/api.py
@@ -4,8 +4,24 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+## Initial Data in my to do list
 
+items = [
 
+    {"id": 1, "name": "Item 1", "description": "This is item 1"},
+    {"id": 2, "name": "Item 2", "description": "This is item 2"}
+]
 
+@app.route('/')
+def home():
+    return "Welcome to The Sample To Do List App"
+
+## Get: Retrieve all items
+
+@app.route("/items", methods=["GET"])
+def get_items():
+    return jsonify(items)
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
