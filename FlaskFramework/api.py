7f024f4e6e2c198068ec50e0963c18a7b0701880
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-09-13 19:51:26 +0530

    add POST code in api.py

--- FlaskFramework/api.py
+++ FlaskFramework/api.py
@@ -22,6 +22,30 @@ def home():
 def get_items():
     return jsonify(items)
 
-    
+## Get: Retrivre a specific item by ID
+
+@app.route('/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item=next((item for item in items if item['id']==item_id),None)
+
+    if item is None:
+        return jsonify({"error":"Item not found"})
+
+    return jsonify(item)       
+
+ ## Post : create a new task
+@app.route('/items'.methods['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error":"item not found"})
+        new_item={
+            "id":items[-1]['id']+1 if items else 1,
+            "name":request.json['name'],
+            "description":request.json['description']
+
+   }
+    items.append(new_item)
+    return jsonify(new_item)
+
 if __name__ == '__main__':
     app.run(debug=True)
