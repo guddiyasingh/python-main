fb841f86c4097972a149fe6829bc1df85b10702e
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-09-20 23:19:28 +0530

    add some code in widgets file

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -4,7 +4,7 @@ st.title("Streamlit Text Input ")
 
 name = st.text_input("Enter your name:")
 
-age = st.text_input("Enter your age:")
+age = st.slider("Enter your age:", 0, 150, 25)
 
 if name:
     st.write(f"Hello, {name}")
