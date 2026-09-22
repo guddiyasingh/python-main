ad2a2c3cd65065b1b2e37efbac31cd9cdca9f7f6
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-09-22 23:04:42 +0530

    add some code in widgets file

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -5,6 +5,10 @@ st.title("Streamlit Text Input ")
 name = st.text_input("Enter your name:")
 
 age = st.slider("Enter your age:", 0, 150, 25)
+st.write(f"Your age is: {age}.")
 
+options = ["python", "java", "c++", "javascript"]
+choice = st.selectbox("Choose your favorite programming language:",options)
+st.write(f"you selected {choice}.")
 if name:
     st.write(f"Hello, {name}")
