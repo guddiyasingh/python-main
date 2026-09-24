ff31e070ee6b7b6c73e87c5d631c1cd576785d82
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-09-24 23:50:17 +0530

    add some code

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -12,3 +12,20 @@ choice = st.selectbox("Choose your favorite programming language:",options)
 st.write(f"you selected {choice}.")
 if name:
     st.write(f"Hello, {name}")
+
+
+data = {
+
+    "Name": ["jhon", "jane", "jake", "jill"],
+    "Age": [25, 30, 35, 40],
+    "City": ["New York", "Los Angles", "Chicago", "Houston"]
+}
+
+df = pd.DataFrame(data)
+st.write(df)
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is note None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
